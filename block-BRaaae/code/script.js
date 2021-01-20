a02ba7c1e8d19f05d92dc7f9b3295e1b55fe06f4
ikushaldave <ikushaldave@gmail.com>
2021-01-20 21:36:56 +0530

    Why React Block 5 Completed

---
+++ block-BRaaae/code/script.js
@@ -0,0 +1,63 @@
+const addMovieToWatchListElement = document.querySelector(".add-movie");
+const watchListElement = document.querySelector(".watch-list");
+
+const watchList = JSON.parse(localStorage.getItem("watchList")) ?? [];
+
+// Functions
+
+function renderUI(watchList) {
+
+	let watchItems = watchList.map((movie, index) => {
+    const li = React.createElement(
+		"li",
+		{
+			className: `watch-movie-item ${movie.isWatched ? "watched" : "to-watch"}`,
+			"data-id": index,
+		},
+		[
+			movie.title,
+			React.createElement(
+				"button",
+				{
+					className: "btn",
+				},
+				[movie.isWatched ? "Watched" : "To Watch"]
+			),
+		]
+    );
+    return li;
+  });
+  
+  ReactDOM.render(watchItems, watchListElement)
+}
+
+function toggle(target) {
+  const id = target.closest("li").dataset.id;
+	watchList[id].isWatched = !watchList[id].isWatched;
+  localStorage.setItem("watchList", JSON.stringify(watchList));
+	renderUI(watchList);
+}
+
+// Event Listener
+
+addMovieToWatchListElement.addEventListener("keyup", (e) => {
+	if (e.keyCode == 13 && e.target.value.trim()) {
+		watchList.push({
+			title: e.target.value.trim(),
+			isWatched: false,
+    });
+    localStorage.setItem("watchList", JSON.stringify(watchList));
+    e.target.value = "";
+		renderUI(watchList);
+	}
+});
+
+watchListElement.addEventListener("click", (e) => {
+	if (e.target.tagName == "BUTTON") {
+		toggle(e.target);
+	}
+});
+
+// Render
+
+renderUI(watchList);
